00b22c08018c862aa720961eae7f186c24a48cdc
Tom <2468775007@qq.com>
2019-06-11 21:59:16 +0800

    33 bisearch

---
+++ 33.SearchInRotatedSortedArray/33-SearchInRotatedSortedArray.py
@@ -0,0 +1,62 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        :note:
+            分两步走：1、第一步log(n)时间找到旋转点i,j；2、第二步分布在旋转点两侧二分查找；总共是3个log(n)时间
+            注意：
+                    —— 当数组没有旋转时：要么只有两个元素，要么i==j，则直接二分查找
+                    —— 当在后半旋转部分查找时，记得索引+j
+        """
+        #nums = [4,5]
+        #target = 5
+        
+        # special case
+        l = len(nums)
+        if nums == []:
+            return -1
+        if l == 1:
+            if nums[0] == target:
+                return 0
+            else:
+                return -1
+        if l==2 and nums[0] < nums[1]:
+                return self.bsearch(nums, target)
+            
+        # find rotate point i, j
+        i, j = 0, l-1
+        while(i != j - 1):
+            m = (i+j)/2
+            if nums[m] > nums[i]:
+                i = m
+            if nums[m] < nums[j]:
+                j = m
+            if i == j:
+                return self.bsearch(nums, target)
+        print i, j  
+        
+        # search half nums
+        if target < nums[0]:
+            ans =  self.bsearch(nums[j:], target)
+            if ans == -1:
+                return ans
+            else:
+                return ans + j
+        else:
+            return self.bsearch(nums[:i+1], target)
+        
+    # binary search
+    def bsearch(self, nums, target):
+        l = len(nums)
+        i, j = 0, l-1
+        while(i<=j):
+            m = (i + j)/2
+            if target == nums[m]:
+                return m
+            elif target > nums[m]:
+                i = m+1
+            else:
+                j = m-1
+        return -1
