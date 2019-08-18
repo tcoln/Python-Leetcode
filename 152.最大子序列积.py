d3c00b7918f40f6eb0a2e0f1f3f57998f1005c7b
Tom <2468775007@qq.com>
2019-08-18 20:44:39 +0800

    O(1)空间的最大子序列积/和

--- 152.最大子序列积.py
+++ 152.最大子序列积.py
@@ -7,23 +7,23 @@ class Solution(object):
             类似于最大子序列和，求以当前元素结尾的最大积。
             但是记录两个数组，分别是当前元素结尾的最大积和最小积，
             负数的时候最小值是与最大值求积，最大值是与最小值求积。
-        """
-        #nums = [-2, 0, -1]
-        if not nums:
+   	美团面试大佬问了，为啥不O(1)空间复杂度     
+	"""
+	if not nums:
             return 0
         l = len(nums)
-        #2 DP
-        maxp = [i for i in nums]
-        minp = [i for i in nums]
+        ans = maxp = minp = nums[0]
         for i in range(1, l):
+            print maxp, minp
             if nums[i] >= 0:
-                maxp[i] = max(maxp[i-1] * nums[i], nums[i])
-                minp[i] = min(minp[i-1] * nums[i], nums[i])
+                maxp = max(maxp * nums[i], nums[i])
+                minp = min(minp * nums[i], nums[i])
             else:
-                maxp[i] = max(minp[i-1] * nums[i], nums[i])
-                minp[i] = min(maxp[i-1] * nums[i], nums[i])
-        print maxp      
-        return max(maxp)
+                tmp = maxp
+                maxp = max(minp * nums[i], nums[i])
+                minp = min(tmp * nums[i], nums[i])
+            ans = max(ans, maxp)
+        return ans
     
         #1 暴力方法
         if not nums:
